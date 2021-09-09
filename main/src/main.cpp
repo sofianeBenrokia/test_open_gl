46cc8645d825c2d267f03482ff07504150591a3d
djadjabelgroune <benrokia.sofiane.adel@gmail.com>
2021-09-09 21:34:56 +0200

    shader include

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,3 +1,9 @@
+/*
+* S_ : is for static
+*
+*
+*/
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -6,9 +12,40 @@
 
 #define LOG(text) std::cout<<text<<std::endl
 
-static unsigned int S_create_shader(std::string vertex_shader , std::string fragment_shader)
+static unsigned int S_compile_shader(const unsigned int TYPE , std::string& source)
     {
+        unsigned int id = glCreateShader(TYPE);
+
+        if(id == 0)
+            LOG("ERROR :creating shader");
         
+        const char* temp_source = source.c_str();
+        
+        glShaderSource(id , 1 , &temp_source , NULL);
+
+        glCompileShader(id);
+
+        return id ;
+    }
+
+static unsigned int S_create_shader(std::string& vertex_shader , std::string& fragment_shader)
+    {
+        unsigned int program = glCreateProgram();
+        unsigned int temp_vertex_shader = S_compile_shader(GL_VERTEX_SHADER , vertex_shader);
+        unsigned int temp_fragment_shader =  S_compile_shader(GL_FRAGMENT_SHADER , fragment_shader);
+
+        glAttachShader(program , temp_vertex_shader);
+        glAttachShader(program , temp_fragment_shader);
+
+        glLinkProgram(program);
+        
+        glValidateProgram(program);
+            
+        glDeleteShader(temp_vertex_shader);
+        glDeleteShader(temp_fragment_shader);
+
+        return program ;
+
     }
 
 int main(void)
@@ -58,6 +95,30 @@ int main(void)
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0 , 2 , GL_FLOAT , GL_FALSE , 2 * sizeof(float) , 0);
 
+        std::string vertex_shader =
+            "#version 330 core\n"
+            "\n"
+            "layout(location = 0) in vec4 triangle_cordoner;\n"
+            "\n"
+            "void main()\n"
+            "{\n"
+            "   gl_Position = triangle_cordoner;\n"
+            "}\n";
+
+        std::string fragment_shader =
+            "#version 330 core\n"
+            "\n"
+            "layout(location = 0) out vec4 color;\n"
+            "\n"
+            "void main()\n"
+            "{\n"
+            "   color = vec4(1.0 , 0.0 , 0.0 , 1.0);\n"
+            "}\n";
+
+        unsigned int shader = S_create_shader(vertex_shader , fragment_shader);
+
+        glUseProgram(shader);
+
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
@@ -73,6 +134,8 @@ int main(void)
             /* Poll for and process events */
             glfwPollEvents();
         }
+
+        glDeleteProgram(shader);
 
         glfwTerminate();
         return 0;
